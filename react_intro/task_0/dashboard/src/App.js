22885a5d8d59379eea63d85cd7f1e2adf2163b56
TonyBarretor <tony.barretor@gmail.com>
2024-03-17 20:26:44 -0700

    feat: task0 complete except for the CSS

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -5,20 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      {/* Header */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="Holberton Logo" />
         <h1>School dashboard</h1>
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+
       </header>
 
       {/* Body */}
