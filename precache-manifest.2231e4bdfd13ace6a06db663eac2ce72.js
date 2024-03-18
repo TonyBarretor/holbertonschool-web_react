4079872239c2052fcae2c3e2526046e88992fe1c
TonyBarretor <tony.barretor@gmail.com>
2024-03-18 01:54:08 -0700

    Updates

---
+++ precache-manifest.2231e4bdfd13ace6a06db663eac2ce72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f35fa4a1715cc89cd007918704197d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc7d4e54bde0712991a9",
+    "url": "/static/css/main.4c7fc75b.chunk.css"
+  },
+  {
+    "revision": "04be10c961ad2b15945a",
+    "url": "/static/js/2.23e6d5d4.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.23e6d5d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11e384180f06397a6e56",
+    "url": "/static/js/3.cc92abf2.chunk.js"
+  },
+  {
+    "revision": "bc7d4e54bde0712991a9",
+    "url": "/static/js/main.8b64d887.chunk.js"
+  },
+  {
+    "revision": "a48fa9ea7c5450582cc5",
+    "url": "/static/js/runtime-main.b1fd81be.js"
+  },
+  {
+    "revision": "23d20eb3253de8404d66401eb6db58d5",
+    "url": "/static/media/holbertonLogo.23d20eb3.jpg"
+  }
+]);
